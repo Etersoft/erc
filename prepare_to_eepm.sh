bd20d8ff67726b1891928e64c264a87e86e78641
Vitaly Lipatov <lav@etersoft.ru>
2023-04-10 03:03:57 +0300

    1.0.1-alt1
    - erc-sh-functions: sync which using
    - ercat: use is_command instead of which
    - erc-sh-functions: improve color functions (don't use tput directly)

--- prepare_to_eepm.sh
+++ prepare_to_eepm.sh
@@ -2,6 +2,6 @@
 subst 's|a/bin/erc$|a/bin/tools_erc|' 000*.patch
 subst 's|b/bin/erc$|b/bin/tools_erc|' 000*.patch
 
-subst 's|a/bin/ercat$|a/bin/tools_erc|' 000*.patch
-subst 's|b/bin/ercat$|b/bin/tools_erc|' 000*.patch
+subst 's|a/bin/ercat$|a/bin/tools_ercat|' 000*.patch
+subst 's|b/bin/ercat$|b/bin/tools_ercat|' 000*.patch
 
